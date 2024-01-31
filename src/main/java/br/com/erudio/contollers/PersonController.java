8217019df25813676d76acd06bce1a25e924bdd6
Rubens dos Santos de Maria <rubenshds@gmail.com>
2024-01-31 07:48:27 -0300

    PersonVO (Value Object) created and implemented

--- src/main/java/br/com/erudio/contollers/PersonController.java
+++ src/main/java/br/com/erudio/contollers/PersonController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.erudio.model.Person;
+import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.services.PersonService;
 
 @RestController
@@ -30,32 +30,32 @@ public class PersonController {
 	@GetMapping(value = "/{id}", 
 				produces = MediaType.APPLICATION_JSON_VALUE) // No RequestMapping seria conveniente explicitar o tipo de requisição, no caso
 								// GET.
-	public Person findPersonByID(@PathVariable(value = "id") Long id)  {
-		return personService.findPersonById(id);
+	public PersonVO findPersonVOByID(@PathVariable(value = "id") Long id)  {
+		return personService.findPersonVOById(id);
 	} // PathVariable associa o valor do parâmetro com o valor da PathVariable(value =
 		// number1) por exemplo.
 
 	@GetMapping(value = "/findAll",
 				produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll(){
+	public List<PersonVO> findAll(){
 		return personService.findAll();
 	}
 
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 				 produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person createPerson(@RequestBody Person person) {
+	public PersonVO createPersonVO(@RequestBody PersonVO person) {
 		return personService.create(person);
 	}
 	
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 				produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person updatePerson(@RequestBody Person person) {
+	public PersonVO updatePersonVO(@RequestBody PersonVO person) {
 		return personService.update(person);
 	}
 	
 	@DeleteMapping(value="/{id}", 
 			       produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> deletePerson(@PathVariable Long id) {
+	public ResponseEntity<?> deletePersonVO(@PathVariable Long id) {
 		personService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
--- src/main/java/br/com/erudio/services/PersonService.java
+++ src/main/java/br/com/erudio/services/PersonService.java
@@ -8,7 +8,9 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.exceptions.ResourceNotFoundException;
+import br.com.erudio.mapper.DozerMapper;
 import br.com.erudio.model.Person;
 import br.com.erudio.repositories.PersonRepository;
 
@@ -19,37 +21,40 @@ public class PersonService implements Serializable{
 	@Autowired
 	private PersonRepository repository;
 	
-	private final AtomicLong cont = new AtomicLong();
+	
 	private Logger logger = Logger.getLogger(PersonService.class.getName());
 	
-	public List<Person> findAll(){
+	public List<PersonVO> findAll(){
 		
 		
-		return repository.findAll();
+		return DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
 	}
 	
 
 
-	public Person findPersonById (Long id) {
+	public PersonVO findPersonVOById (Long id) {
 		logger.info("Finding one person");
 		
-		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("There was no person with this ID: " + id)); 
+		var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("There was no person with this ID: " + id)); 
+		return DozerMapper.parseObject(entity, PersonVO.class);
 	}
 	
-	public Person create(Person person) {
+	public PersonVO create(PersonVO person) {
 		logger.info("Creating a person");
-		
-		return repository.save(person);
+		var entity = DozerMapper.parseObject(person, Person.class);
+		var vo = DozerMapper.parseObject(repository.save(entity),PersonVO.class);
+		 return vo;
 	}
 
-	public Person update(Person person) {
+	public PersonVO update(PersonVO person) {
 		logger.info("Updating a person");
-		var entity = findPersonById(person.getId());
-		entity.setName(person.getName());
+		var entity = repository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundException("There was no person with this ID:"));
+		entity.setFirstName(person.getFirstName());
 		entity.setLastName(person.getLastName());
 		entity.setAddress(person.getAddress());
 		entity.setGender(person.getGender());
-		return repository.save(entity);
+		
+		return DozerMapper.parseObject(entity, PersonVO.class);
 	}
 	
 	
